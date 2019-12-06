e69cbd4c4e8219c94fc3763443056860fd051ef9
snehapatil11 <sneha.magdum11@gmail.com>
2019-12-06 13:28:03 -0800

    Update API Endpoint Url

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ const expect = require('chai').expect;
 require("../index.js");
 
 it('Check get reviews by business id is working', function(done) {
-    axios.get("localhost:3000/businesses/02151fdb-8b50-4068-b8a1-1d24ed425c59/reviews")
+    axios.get("https://reviewsvc.codeninjas.cf/businesses/02151fdb-8b50-4068-b8a1-1d24ed425c59/reviews")
         .then((res) => {
             expect(res.status).to.equal(200);
             done();
